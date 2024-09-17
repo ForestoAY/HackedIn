13d64211a6f0bc69bd346b6e0724da7a4d7889af
ForestoAY <forestoay@gmail.com>
2024-09-17 15:59:36 +0700

    refactor: export database on mongodb

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -9,14 +9,6 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
-async function run() {
-  try {
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    await client.close();
-  }
-}
-run().catch(console.dir);
+
+const database = client.db("gc1p3");
+module.exports = database;
