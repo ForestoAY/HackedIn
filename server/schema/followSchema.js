dec5508b0d273c1e657cac49cadb1f348fd8fdcb
ForestoAY <forestoay@gmail.com>
2024-09-18 12:37:19 +0700

    add auth on followUser

--- server/schema/followSchema.js
+++ server/schema/followSchema.js
@@ -1,23 +1,29 @@
 const Follow = require("../models/Follow");
+const User = require("../models/User");
 
 const followTypeDefs = `#graphql
   type Follow {
     _id: String
-    followingId: String
-    followerId: String
+    followingId: ID
+    followerId: ID
     createdAt: String
     updatedAt: String
   }
 
   type Mutation {
-    followUser(followingId: String!, followerId: String!): Follow
+    followUser(followingId: ID, followerId: ID): Follow
   }
 `;
 
 const followResolvers = {
   Mutation: {
-    followUser: async (_, args) => {
-      const { followingId, followerId } = args;
+    followUser: async (_, args, contextValue) => {
+      const user = await contextValue.auth();
+      let { followingId, followerId } = args;
+      followerId = user._id;
+
+      const followedUser = await User.getUserById(followingId);
+      if (!followedUser) throw new Error("User not found");
 
       const result = await Follow.addFollow(followingId, followerId);
 
