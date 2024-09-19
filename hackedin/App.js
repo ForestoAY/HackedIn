ceb147c0484b4aec8cf53f0768fed7ff575a919f
ForestoAY <forestoay@gmail.com>
2024-09-19 15:42:32 +0700

    feat: add searchPage

--- hackedin/App.js
+++ hackedin/App.js
@@ -3,13 +3,25 @@ import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import AddPost from "./pages/AddPost";
 import DetailPage from "./pages/DetailPage";
+import SearchPage from "./pages/SearchPage";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomePage} />
+      <Tab.Screen name="Search" component={SearchPage} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -17,24 +29,18 @@ export default function App() {
       <SafeAreaView style={{ height: "100%" }}>
         <NavigationContainer>
           <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomePage} />
+            <Stack.Screen
+              name="Home"
+              component={HomeTabs}
+              options={{ headerShown: false }}
+            />
             <Stack.Screen name="Add" component={AddPost} />
             <Stack.Screen name="Detail" component={DetailPage} />
             {/* <LoginPage /> */}
             {/* <RegisterPage /> */}
-            {/* <HomePage /> */}
           </Stack.Navigator>
         </NavigationContainer>
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ hackedin/pages/SearchPage.js
@@ -0,0 +1,39 @@
+import { View, Text, StyleSheet, TextInput } from "react-native";
+
+export default function SearchPage() {
+  return (
+    <>
+      <View>
+        <TextInput
+          style={{
+            marginVertical: 8,
+            height: 50,
+            marginHorizontal: 12,
+            paddingHorizontal: 20,
+            borderColor: "#83B4FF",
+            borderWidth: 1,
+            borderRadius: 7,
+          }}
+          placeholder="Search users"
+          secureTextEntry
+          autoCorrect={false}
+          autoCapitalize="none"
+        />
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.text}>User here</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginVertical: 8,
+    marginHorizontal: 12,
+  },
+  text: {
+    fontSize: 20,
+  },
+});
