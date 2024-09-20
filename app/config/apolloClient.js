eeb5db4560f7ea652c4aafd421b4a309d38c3f7e
ForestoAY <forestoay@gmail.com>
2024-09-20 16:58:17 +0700

    refactor: setup apolloclient

--- app/config/apolloClient.js
+++ app/config/apolloClient.js
@@ -1,7 +1,23 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { getItemAsync } from "expo-secure-store";
+
+const httpLink = createHttpLink({
+  uri: "http://192.168.1.6:3000/",
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await getItemAsync("access_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const apolloClient = new ApolloClient({
-  uri: "http://192.168.1.6:3000/",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
