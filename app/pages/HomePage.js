add70593316bbb51dbe548abf7c22060a5287c85
ForestoAY <forestoay@gmail.com>
2024-09-21 15:09:13 +0700

    fix: fix all logic on AddLike post

--- app/pages/HomePage.js
+++ app/pages/HomePage.js
@@ -8,11 +8,37 @@ import {
   View,
 } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
-import { useQuery } from "@apollo/client";
-import { GET_POSTS } from "../apollo/postsOperation";
+import { useMutation, useQuery } from "@apollo/client";
+import { ADD_LIKE, GET_POSTS } from "../apollo/postsOperation";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/auth";
 
 export default function HomePage({ navigation }) {
+  const authContext = useContext(AuthContext);
   const { loading, error, data } = useQuery(GET_POSTS);
+  const [likedPosts, setLikedPosts] = useState({});
+
+  const [addLike] = useMutation(ADD_LIKE, {
+    refetchQueries: [GET_POSTS],
+  });
+
+  useEffect(() => {
+    if (data) {
+      const newLikedPosts = {};
+      console.log(authContext.user?._id);
+
+      data.posts.forEach((post) => {
+        const likes = post.likes;
+        newLikedPosts[post._id] = likes.some(
+          (like) => like._id === authContext.user?._id
+        );
+      });
+      console.log(newLikedPosts, "🍕🍕");
+
+      setLikedPosts(newLikedPosts);
+    }
+  }, [data, authContext.user?._id]);
+
   if (loading) {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -28,83 +54,90 @@ export default function HomePage({ navigation }) {
       </View>
     );
   }
+
   return (
-    <>
-      <View style={styles.posts}>
-        <FlatList
-          data={data.posts}
-          keyExtractor={(post) => post._id}
-          renderItem={(info) => {
-            const { item } = info;
-            return (
-              <View style={styles.postContainer}>
-                <Text
-                  style={styles.username}
-                  onPress={() => {
-                    navigation.push("ProfilePage", {
-                      id: item.author._id,
-                    });
-                  }}
-                >
-                  {item.author.username}
+    <View style={styles.posts}>
+      <FlatList
+        data={data.posts}
+        keyExtractor={(post) => post._id}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.postContainer}>
+              <Text
+                style={styles.username}
+                onPress={() => {
+                  navigation.push("ProfilePage", { id: item.author._id });
+                }}
+              >
+                {item.author.username}
+              </Text>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.push("Detail", { postId: item._id });
+                }}
+              >
+                <View>
+                  <Text style={styles.content}>{item.content}</Text>
+                  <Image source={{ uri: item.imgUrl }} style={styles.image} />
+                </View>
+              </TouchableOpacity>
+
+              <View style={styles.tagContainer}>
+                {item.tags.map((tag, index) => (
+                  <TouchableOpacity key={index} style={styles.tagButton}>
+                    <Text style={styles.tagText}>{tag}</Text>
+                  </TouchableOpacity>
+                ))}
+              </View>
+
+              <View style={styles.infoContainer}>
+                <Text style={styles.infoText}>{item.likes.length} likes</Text>
+                <Text style={styles.infoText}>
+                  {item.comments.length} comments
                 </Text>
+              </View>
+              <View style={styles.actionContainer}>
                 <TouchableOpacity
                   onPress={() => {
-                    navigation.push("Detail", {
-                      postId: item._id,
-                    });
+                    addLike({
+                      variables: { postId: item._id, newLike: {} },
+                    })
+                      .then(() => {
+                        setLikedPosts((prev) => ({
+                          ...prev,
+                          [item._id]: !prev[item._id],
+                        }));
+                      })
+                      .catch((err) => {
+                        console.error("Error adding like:", err);
+                      });
                   }}
                 >
-                  <View>
-                    <Text style={styles.content}>{item.content}</Text>
-                    <Image source={{ uri: item.imgUrl }} style={styles.image} />
-                  </View>
+                  <Icon
+                    name={likedPosts[item._id] ? "thumbs-up" : "thumbs-o-up"}
+                    size={32}
+                    color="black"
+                    style={styles.icon}
+                  />
                 </TouchableOpacity>
-
-                <View style={styles.tagContainer}>
-                  {item.tags.map((tag, index) => (
-                    <TouchableOpacity key={index} style={styles.tagButton}>
-                      <Text style={styles.tagText}>{tag}</Text>
-                    </TouchableOpacity>
-                  ))}
-                </View>
-
-                <View style={styles.infoContainer}>
-                  <Text style={styles.infoText}>{item.likes.length} likes</Text>
-                  <Text style={styles.infoText}>
-                    {item.comments.length} comments
-                  </Text>
-                </View>
-                <View style={styles.actionContainer}>
-                  <TouchableOpacity>
-                    <Icon
-                      name="thumbs-o-up"
-                      size={32}
-                      color="black"
-                      style={styles.icon}
-                    />
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    onPress={() => {
-                      navigation.navigate("Detail", {
-                        postId: item._id,
-                      });
-                    }}
-                  >
-                    <Icon
-                      name="comment-o"
-                      size={32}
-                      color="black"
-                      style={styles.icon}
-                    />
-                  </TouchableOpacity>
-                </View>
+                <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate("Detail", { postId: item._id });
+                  }}
+                >
+                  <Icon
+                    name="comment-o"
+                    size={32}
+                    color="black"
+                    style={styles.icon}
+                  />
+                </TouchableOpacity>
               </View>
-            );
-          }}
-        />
-      </View>
-    </>
+            </View>
+          );
+        }}
+      />
+    </View>
   );
 }
 
