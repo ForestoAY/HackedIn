135a5e91cbf9d2714a9b4250966adc8f26af71fb
ForestoAY <forestoay@gmail.com>
2024-09-22 07:54:44 +0700

    style: add user who login in logoutpage

--- app/pages/LogoutPage.js
+++ app/pages/LogoutPage.js
@@ -5,28 +5,44 @@ import { deleteItemAsync } from "expo-secure-store";
 
 export default function LogoutPage() {
   const authContext = useContext(AuthContext);
+  const user = authContext.user;
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <TouchableOpacity
-        style={{
-          marginVertical: 8,
-          backgroundColor: "red",
-          width: 100,
-          height: 45,
-          borderRadius: 5,
-          marginHorizontal: 12,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-        onPress={async () => {
-          await deleteItemAsync("access_token");
-          authContext.setIsSignedIn(false);
-          authContext.setUser(null);
-        }}
-      >
-        <Text style={{ color: "white" }}>Logout</Text>
-      </TouchableOpacity>
+      <View style={{ alignItems: "center" }}>
+        <Text
+          style={{
+            fontSize: 28,
+            marginVertical: 8,
+            fontWeight: "600",
+          }}
+        >
+          {user?.username}
+        </Text>
+        <Text style={{ fontSize: 18, marginBottom: 20 }}>
+          Name: {user?.name}
+        </Text>
+
+        <TouchableOpacity
+          style={{
+            marginVertical: 8,
+            backgroundColor: "red",
+            width: 100,
+            height: 45,
+            borderRadius: 5,
+            marginHorizontal: 12,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onPress={async () => {
+            await deleteItemAsync("access_token");
+            authContext.setIsSignedIn(false);
+            authContext.setUser(null);
+          }}
+        >
+          <Text style={{ color: "white" }}>Logout</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
